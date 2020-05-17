581cfc15380f32e470f960720501df39fc6bdac3
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-05-17 03:54:40 -0400

    blog post html roughly complete

--- hackathon/blog/views.py
+++ hackathon/blog/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 
+posts = [
+
+    {
+        'author': 'Nathan',
+        'title': 'Test 1',
+        'content': 'LOLOLOLOOLOLOL',
+        'date': 'Now' 
+    },
+
+    {
+        'author': 'Not Nathan',
+        'title': 'Test 2',
+        'content': 'Not LOLOLOLOOLOLOL',
+        'date': 'Not Now' 
+    }
+
+]
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/base.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
