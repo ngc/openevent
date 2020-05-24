ce8396c3c81d53e3718f43e4adef619e69fd162f
thaniel-c <44456832+thaniel-c@users.noreply.github.com>
2020-05-24 18:59:14 -0400

    fix profile password update error

--- hackathon/users/views.py
+++ hackathon/users/views.py
@@ -164,7 +164,7 @@ def change_password(request):
             user = form.save()
             update_session_auth_hash(request, user) 
             messages.success(request, 'Your password was successfully updated!')
-            return redirect('change_password')
+            return redirect('../profile')
         else:
             messages.error(request, 'Please correct the error below.')
     else:
